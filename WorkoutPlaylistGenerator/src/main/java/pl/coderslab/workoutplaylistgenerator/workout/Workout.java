7de2857767dcd7c7b813140537f05419a96d7a31
malgorzatakalinowska <malgorzatakalinowska100@wp.pl>
2023-05-08 22:17:46 +0200

    Added Enum

--- WorkoutPlaylistGenerator/src/main/java/pl/coderslab/workoutplaylistgenerator/workout/Workout.java
+++ WorkoutPlaylistGenerator/src/main/java/pl/coderslab/workoutplaylistgenerator/workout/Workout.java
@@ -3,27 +3,28 @@ package pl.coderslab.workoutplaylistgenerator.workout;
 import lombok.*;
 import lombok.AllArgsConstructor;
 import org.hibernate.validator.constraints.Range;
+import org.springframework.stereotype.Component;
 import pl.coderslab.workoutplaylistgenerator.user.User;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="workouts")
+@Builder
+@Component
 public class Workout {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, name = "workout_id")
     private Long id;
 
-    @NotBlank
-    @Size(min=3, max=15)
+    @Enumerated(EnumType.STRING)
     @Column(name = "type")
-    private String type;
+    private WorkoutType type;
 
     @Range(min=1, max=10)
     @Column(name = "intensity")
@@ -32,5 +33,4 @@ public class Workout {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
 }
---
+++ WorkoutPlaylistGenerator/src/main/java/pl/coderslab/workoutplaylistgenerator/workout/WorkoutType.java
@@ -0,0 +1,8 @@
+package pl.coderslab.workoutplaylistgenerator.workout;
+
+public enum WorkoutType {
+        Zumba,
+        Cycling,
+        Running,
+        Joga
+}
