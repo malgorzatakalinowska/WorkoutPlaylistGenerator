0a28938fdbbebc3cd069846b6567ff1d5b3f82c3
malgorzatakalinowska <malgorzatakalinowska100@wp.pl>
2023-05-10 00:43:52 +0200

    Changed methods

--- WorkoutPlaylistGenerator/src/main/java/pl/coderslab/workoutplaylistgenerator/workout/WorkoutService.java
+++ WorkoutPlaylistGenerator/src/main/java/pl/coderslab/workoutplaylistgenerator/workout/WorkoutService.java
@@ -2,34 +2,50 @@ package pl.coderslab.workoutplaylistgenerator.workout;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.webjars.NotFoundException;
 import pl.coderslab.workoutplaylistgenerator.exception.IdMismatchException;
 import pl.coderslab.workoutplaylistgenerator.exception.ResourceNotFoundException;
+import pl.coderslab.workoutplaylistgenerator.user.User;
+import pl.coderslab.workoutplaylistgenerator.user.UserRepository;
 
 import java.util.List;
+
 
 @Service
 public class WorkoutService {
 
     private final WorkoutRepository workoutRepository;
     private final WorkoutMapper workoutMapper;
+    private final UserRepository userRepository;
+    private final User user;
 
-    public WorkoutService(WorkoutRepository workoutRepository, WorkoutMapper workoutMapper) {
+    public WorkoutService(WorkoutRepository workoutRepository, WorkoutMapper workoutMapper, UserRepository userRepository, User user) {
         this.workoutRepository = workoutRepository;
         this.workoutMapper = workoutMapper;
+        this.userRepository = userRepository;
+        this.user = user;
     }
 
-    public WorkoutDto createWorkout(WorkoutDto dto) {
-        Workout workout = workoutMapper.mapToEntity(dto);
-        Assert.isNull(workout.getId(), "Id has to be null");
-        workoutRepository.save(workout);
-        return workoutMapper.mapToDto(workout);
+    public WorkoutDto createWorkout(WorkoutDto workoutDto) {
+        if (workoutDto.getUserId() == null) {
+            throw new IllegalArgumentException("User ID cannot be null");
+        } else {
+            workoutDto.setUser_id(workoutDto.getUserId());
+        }
+        User user = userRepository.findById(workoutDto.getUserId())
+                .orElseThrow(() -> new NotFoundException("User not found"));
+        Workout workout = workoutMapper.mapToEntity(workoutDto);
+        workout.setUser(user);
+        workout = workoutRepository.save(workout);
+        workoutDto = workoutMapper.mapToDto(workout);
+        workoutDto.setUser_id(user.getId());
+        return workoutDto;
     }
 
     public WorkoutDto getWorkout(Long id) {
-        return workoutMapper.mapToDto(workoutRepository.findById(id).orElse(null));
-    }
+        return workoutMapper.mapToDto(workoutRepository.findById(id).orElse(null));    }
 
-    public List<WorkoutDto> getAllWorkouts() {
+    public List<WorkoutDto> getAllWorkouts(){
         return workoutMapper.mapToDto(workoutRepository.findAll());
     }
 
