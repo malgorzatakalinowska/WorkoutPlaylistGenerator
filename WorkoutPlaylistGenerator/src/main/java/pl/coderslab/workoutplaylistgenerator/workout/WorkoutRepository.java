ba296e4a71d2dabe8013899978fe9efa861d6882
malgorzatakalinowska <malgorzatakalinowska100@wp.pl>
2023-05-10 00:43:33 +0200

    -

--- WorkoutPlaylistGenerator/src/main/java/pl/coderslab/workoutplaylistgenerator/workout/WorkoutRepository.java
+++ WorkoutPlaylistGenerator/src/main/java/pl/coderslab/workoutplaylistgenerator/workout/WorkoutRepository.java
@@ -1,6 +1,10 @@
 package pl.coderslab.workoutplaylistgenerator.workout;
 
+import org.hibernate.jdbc.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+
 }
